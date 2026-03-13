6c907526d6c6e12d591eb9214652c5ad4085db06
gnmancia <geraldnoelmancia@gmail.com>
2026-03-13 18:39:06 +0800

    Modify calculator UI and initial values
    Updated initial values and label colors for better clarity.

--- Mancia_HO3.py
+++ Mancia_HO3.py
@@ -20,17 +20,17 @@ window = Tk()
 window.title("Hi Mancia, Simple Calculator")
 window.geometry("205x205")   
 
-label_result = Label(window, text="The sum is of 4 + 5 is 9.")
+label_result = Label(window, text="The sum is of 15 + 20 is 35.")
 label_result.pack(pady=5)
 
 frame = Frame(window, bg="gray")
 frame.pack(fill="both", expand=True)
 
-Label(frame, text="Enter 1st Number:", bg="white").grid(row=0, column=0, padx=5, pady=3)
+Label(frame, text="Enter 1st Number:", bg="lightyellow").grid(row=0, column=0, padx=5, pady=3)
 num1 = Entry(frame, width=12)
 num1.grid(row=0, column=1)
 
-Label(frame, text="Enter 2nd Number:", bg="white").grid(row=1, column=0, padx=5, pady=3)
+Label(frame, text="Enter 2nd Number:", bg="lightyellow").grid(row=1, column=0, padx=5, pady=3)
 num2 = Entry(frame, width=12)
 num2.grid(row=1, column=1)
 
